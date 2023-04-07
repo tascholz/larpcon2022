02357177b28c8e02b0174a2d27feb0cee5244ad3
Timo Scholz <timo.scholz@senagon.com>
2023-04-07 17:07:42 +0200

    fix: mobile menu

--- public/js/app.js
+++ public/js/app.js
@@ -6026,6 +6026,71 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   data: function data() {
     return {
@@ -6057,6 +6122,22 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
           }
         }, _callee);
       }))();
+    },
+    MenuOpen: function MenuOpen() {
+      var menuburger = document.querySelector('#menuburger');
+      var menu = document.querySelector('#menu');
+
+      if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+      } else {
+        menu.classList.add('hidden');
+      }
+
+      open.value = !open.value;
+    },
+    LinkOpen: function LinkOpen() {
+      var menu = document.querySelector('#menu');
+      menu.classList.add('hidden');
     }
   },
   created: function created() {
@@ -12180,7 +12261,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.nav-bg[data-v-df60c04c] {\n    background-color: rgb(55, 87, 57);\n    color: white;\n}\n.nav-custom[data-v-df60c04c] {\n    color: white;\n}\n.nav-custom[data-v-df60c04c]:hover {\n    color:white;\n    cursor: pointer;\n    text-shadow:\n        0 0 7px #fff,\n        0 0 10px #fff,\n        0 0 21px #fff,\n        0 0 42px #0fa,\n        0 0 82px #0fa;\n}\n.background[data-v-df60c04c] {\n    height: 100vh;\n    padding: 20px;\n    background-color: rgb(55, 87, 57);\n    background-image: url(\"/img/bg.jpg\");\n    background-size: cover;\n}\n.content[data-v-df60c04c] {\n    background-color: rgba(55, 87, 57, 0.774);\n    margin-left: auto;\n    margin-right: auto;\n    color: white;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.nav-bg[data-v-df60c04c] {\n    background-color: rgb(55, 87, 57);\n    color: white;\n}\n.nav-custom[data-v-df60c04c] {\n    color: white;\n}\n.nav-custom[data-v-df60c04c]:hover {\n    color:white;\n    cursor: pointer;\n    text-shadow:\n        0 0 7px #fff,\n        0 0 10px #fff,\n        0 0 21px #fff,\n        0 0 42px #0fa,\n        0 0 82px #0fa;\n}\n.background[data-v-df60c04c] {\n    height: 100vh;\n    padding: 20px;\n    background-color: rgb(55, 87, 57);\n    background-image: url(\"/img/bg.jpg\");\n    background-size: cover;\n}\n.content[data-v-df60c04c] {\n    background-color: rgba(55, 87, 57, 0.774);\n    margin-left: auto;\n    margin-right: auto;\n    color: white;\n}\n.hidden[data-v-df60c04c] {\n    display: none;\n}\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
@@ -32744,12 +32825,6 @@ var render = function () {
                       ),
                     ]),
                     _vm._v(" "),
-                    _c("p", [
-                      _vm._v(
-                        "Falls du NSC sein möchtest, überspringe den IT-Teil"
-                      ),
-                    ]),
-                    _vm._v(" "),
                     _c("div", { staticClass: "form-check" }, [
                       _c("input", {
                         directives: [
@@ -32790,6 +32865,10 @@ var render = function () {
                     ]),
                   ])
                 : _vm._e(),
+              _vm._v(" "),
+              _c("p", [
+                _vm._v("Wenn du NSC zein möchtest, überspringe den IT-Teil"),
+              ]),
               _vm._v(" "),
               _c("hr"),
               _vm._v(" "),
@@ -33204,7 +33283,7 @@ var render = function () {
               _vm.event.reg_newsletter == 1
                 ? _c("div", { staticClass: "form-group" }, [
                     _c("label", { attrs: { for: "newsletter" } }, [
-                      _vm._v("Möchtest du unseren Newsletter abbonieren?"),
+                      _vm._v("Möchtest du unseren Newsletter abonieren?"),
                     ]),
                     _vm._v(" "),
                     _c("div", { staticClass: "form-check" }, [
@@ -33596,121 +33675,131 @@ var render = function () {
   return _c("div", [
     _c(
       "nav",
-      { staticClass: "navbar navbar-expand-sm  nav-bg" },
+      { staticClass: "navbbar navbbar-bg" },
       [
         _c(
           "router-link",
           {
-            staticClass: "navbar-brand ml-4 nav-custom",
+            staticClass: "navbar-brand ml-3 nav-custom",
             attrs: { to: { name: "home" } },
           },
           [_vm._v("Larpcons")]
         ),
         _vm._v(" "),
-        _vm._m(0),
-        _vm._v(" "),
         _c(
-          "div",
-          {
-            staticClass: "collapse navbar-collapse",
-            attrs: { id: "navbarsExample03" },
-          },
+          "button",
+          { staticClass: "navbar-toggler", attrs: { type: "button" } },
           [
-            _c("ul", { staticClass: "navbar-nav mr-auto" }, [
-              _c(
-                "li",
-                { staticClass: "nav-item" },
-                [
-                  _c(
-                    "router-link",
-                    {
-                      staticClass: "nav-link nav-custom",
-                      attrs: { to: { name: "calendar" } },
-                    },
-                    [_vm._v("Kalender")]
-                  ),
-                ],
-                1
-              ),
-              _vm._v(" "),
-              _c(
-                "li",
-                { staticClass: "nav-item" },
-                [
-                  _vm.user
-                    ? _c(
-                        "router-link",
-                        {
-                          staticClass: "nav-link nav-custom",
-                          attrs: { to: { name: "user" } },
-                        },
-                        [_vm._v("Spieler-Bereich")]
-                      )
-                    : _c(
-                        "router-link",
-                        {
-                          staticClass: "nav-link disabled",
-                          attrs: { to: { name: "user" } },
-                        },
-                        [_vm._v("Spieler-Bereich")]
-                      ),
-                ],
-                1
-              ),
-              _vm._v(" "),
-              _c(
-                "li",
-                { staticClass: "nav-item" },
-                [
-                  _vm.orga_id
-                    ? _c(
-                        "router-link",
-                        {
-                          staticClass: "nav-link nav-custom",
-                          attrs: { to: { name: "orga" } },
-                        },
-                        [_vm._v("Orga-Bereich")]
-                      )
-                    : _c(
-                        "router-link",
-                        {
-                          staticClass: "nav-link disabled",
-                          attrs: { to: { name: "orga" } },
-                        },
-                        [_vm._v("Orga-Bereich")]
-                      ),
-                ],
-                1
-              ),
-              _vm._v(" "),
-              _c(
-                "li",
-                { staticClass: "nav-item" },
-                [
-                  _vm.user
-                    ? _c(
-                        "a",
-                        {
-                          staticClass: "nav-link nav-custom",
-                          attrs: { to: { name: "home" } },
-                          on: { click: _vm.logout },
-                        },
-                        [_vm._v("\n                    Logout")]
-                      )
-                    : _c(
-                        "router-link",
-                        {
-                          staticClass: "nav-link nav-custom",
-                          attrs: { to: { name: "login" } },
-                        },
-                        [_vm._v("Login")]
-                      ),
-                ],
-                1
-              ),
-            ]),
+            _c(
+              "a",
+              {
+                attrs: {
+                  id: "menuburger",
+                  fill: "none",
+                  stroke: "currentColor",
+                },
+                on: { click: _vm.MenuOpen },
+              },
+              [_vm._v(">")]
+            ),
           ]
         ),
+        _vm._v(" "),
+        _c("div", { staticClass: "hidden", attrs: { id: "menu" } }, [
+          _c("ul", { staticClass: "navbar-nav mr-auto" }, [
+            _c(
+              "li",
+              { staticClass: "nav-item" },
+              [
+                _c(
+                  "router-link",
+                  {
+                    staticClass: "nav-link nav-custom",
+                    attrs: { to: { name: "calendar" } },
+                  },
+                  [_vm._v("Kalender")]
+                ),
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c(
+              "li",
+              { staticClass: "nav-item" },
+              [
+                _vm.user
+                  ? _c(
+                      "router-link",
+                      {
+                        staticClass: "nav-link nav-custom",
+                        attrs: { to: { name: "user" } },
+                      },
+                      [_vm._v("Spieler-Bereich")]
+                    )
+                  : _c(
+                      "router-link",
+                      {
+                        staticClass: "nav-link disabled",
+                        attrs: { to: { name: "user" } },
+                      },
+                      [_vm._v("Spieler-Bereich")]
+                    ),
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c(
+              "li",
+              { staticClass: "nav-item" },
+              [
+                _vm.orga_id
+                  ? _c(
+                      "router-link",
+                      {
+                        staticClass: "nav-link nav-custom",
+                        attrs: { to: { name: "orga" } },
+                      },
+                      [_vm._v("Orga-Bereich")]
+                    )
+                  : _c(
+                      "router-link",
+                      {
+                        staticClass: "nav-link disabled",
+                        attrs: { to: { name: "orga" } },
+                      },
+                      [_vm._v("Orga-Bereich")]
+                    ),
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c(
+              "li",
+              { staticClass: "nav-item" },
+              [
+                _vm.user
+                  ? _c(
+                      "a",
+                      {
+                        staticClass: "nav-link nav-custom",
+                        attrs: { to: { name: "home" } },
+                        on: { click: _vm.logout },
+                      },
+                      [_vm._v("\n                        Logout")]
+                    )
+                  : _c(
+                      "router-link",
+                      {
+                        staticClass: "nav-link nav-custom",
+                        attrs: { to: { name: "login" } },
+                      },
+                      [_vm._v("Login")]
+                    ),
+              ],
+              1
+            ),
+          ]),
+        ]),
       ],
       1
     ),
@@ -33725,28 +33814,7 @@ var render = function () {
     ]),
   ])
 }
-var staticRenderFns = [
-  function () {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c(
-      "button",
-      {
-        staticClass: "navbar-toggler",
-        attrs: {
-          type: "button",
-          "data-toggle": "collapse",
-          "data-target": "#navbarsExample03",
-          "aria-controls": "navbarsExample03",
-          "aria-expanded": "false",
-          "aria-label": "Toggle navigation",
-        },
-      },
-      [_c("span", { staticClass: "navbar-toggler-icon" })]
-    )
-  },
-]
+var staticRenderFns = []
 render._withStripped = true
 
 
@@ -52298,7 +52366,7 @@ Vue.compile = compileToFunctions;
 /***/ ((module) => {
 
 "use strict";
-module.exports = JSON.parse('{"_args":[["axios@0.21.4","/www/htdocs/w01b73e9/larpcons.de"]],"_development":true,"_from":"axios@0.21.4","_id":"axios@0.21.4","_inBundle":false,"_integrity":"sha512-ut5vewkiu8jjGBdqpM44XxjuCjq9LAKeHVmoVfHVzy8eHgxxq8SbAVQNovDA8mVi05kP0Ea/n/UzcSHcTJQfNg==","_location":"/axios","_phantomChildren":{},"_requested":{"type":"version","registry":true,"raw":"axios@0.21.4","name":"axios","escapedName":"axios","rawSpec":"0.21.4","saveSpec":null,"fetchSpec":"0.21.4"},"_requiredBy":["#DEV:/"],"_resolved":"https://registry.npmjs.org/axios/-/axios-0.21.4.tgz","_spec":"0.21.4","_where":"/www/htdocs/w01b73e9/larpcons.de","author":{"name":"Matt Zabriskie"},"browser":{"./lib/adapters/http.js":"./lib/adapters/xhr.js"},"bugs":{"url":"https://github.com/axios/axios/issues"},"bundlesize":[{"path":"./dist/axios.min.js","threshold":"5kB"}],"dependencies":{"follow-redirects":"^1.14.0"},"description":"Promise based HTTP client for the browser and node.js","devDependencies":{"coveralls":"^3.0.0","es6-promise":"^4.2.4","grunt":"^1.3.0","grunt-banner":"^0.6.0","grunt-cli":"^1.2.0","grunt-contrib-clean":"^1.1.0","grunt-contrib-watch":"^1.0.0","grunt-eslint":"^23.0.0","grunt-karma":"^4.0.0","grunt-mocha-test":"^0.13.3","grunt-ts":"^6.0.0-beta.19","grunt-webpack":"^4.0.2","istanbul-instrumenter-loader":"^1.0.0","jasmine-core":"^2.4.1","karma":"^6.3.2","karma-chrome-launcher":"^3.1.0","karma-firefox-launcher":"^2.1.0","karma-jasmine":"^1.1.1","karma-jasmine-ajax":"^0.1.13","karma-safari-launcher":"^1.0.0","karma-sauce-launcher":"^4.3.6","karma-sinon":"^1.0.5","karma-sourcemap-loader":"^0.3.8","karma-webpack":"^4.0.2","load-grunt-tasks":"^3.5.2","minimist":"^1.2.0","mocha":"^8.2.1","sinon":"^4.5.0","terser-webpack-plugin":"^4.2.3","typescript":"^4.0.5","url-search-params":"^0.10.0","webpack":"^4.44.2","webpack-dev-server":"^3.11.0"},"homepage":"https://axios-http.com","jsdelivr":"dist/axios.min.js","keywords":["xhr","http","ajax","promise","node"],"license":"MIT","main":"index.js","name":"axios","repository":{"type":"git","url":"git+https://github.com/axios/axios.git"},"scripts":{"build":"NODE_ENV=production grunt build","coveralls":"cat coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js","examples":"node ./examples/server.js","fix":"eslint --fix lib/**/*.js","postversion":"git push && git push --tags","preversion":"npm test","start":"node ./sandbox/server.js","test":"grunt test","version":"npm run build && grunt version && git add -A dist && git add CHANGELOG.md bower.json package.json"},"typings":"./index.d.ts","unpkg":"dist/axios.min.js","version":"0.21.4"}');
+module.exports = JSON.parse('{"name":"axios","version":"0.21.4","description":"Promise based HTTP client for the browser and node.js","main":"index.js","scripts":{"test":"grunt test","start":"node ./sandbox/server.js","build":"NODE_ENV=production grunt build","preversion":"npm test","version":"npm run build && grunt version && git add -A dist && git add CHANGELOG.md bower.json package.json","postversion":"git push && git push --tags","examples":"node ./examples/server.js","coveralls":"cat coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js","fix":"eslint --fix lib/**/*.js"},"repository":{"type":"git","url":"https://github.com/axios/axios.git"},"keywords":["xhr","http","ajax","promise","node"],"author":"Matt Zabriskie","license":"MIT","bugs":{"url":"https://github.com/axios/axios/issues"},"homepage":"https://axios-http.com","devDependencies":{"coveralls":"^3.0.0","es6-promise":"^4.2.4","grunt":"^1.3.0","grunt-banner":"^0.6.0","grunt-cli":"^1.2.0","grunt-contrib-clean":"^1.1.0","grunt-contrib-watch":"^1.0.0","grunt-eslint":"^23.0.0","grunt-karma":"^4.0.0","grunt-mocha-test":"^0.13.3","grunt-ts":"^6.0.0-beta.19","grunt-webpack":"^4.0.2","istanbul-instrumenter-loader":"^1.0.0","jasmine-core":"^2.4.1","karma":"^6.3.2","karma-chrome-launcher":"^3.1.0","karma-firefox-launcher":"^2.1.0","karma-jasmine":"^1.1.1","karma-jasmine-ajax":"^0.1.13","karma-safari-launcher":"^1.0.0","karma-sauce-launcher":"^4.3.6","karma-sinon":"^1.0.5","karma-sourcemap-loader":"^0.3.8","karma-webpack":"^4.0.2","load-grunt-tasks":"^3.5.2","minimist":"^1.2.0","mocha":"^8.2.1","sinon":"^4.5.0","terser-webpack-plugin":"^4.2.3","typescript":"^4.0.5","url-search-params":"^0.10.0","webpack":"^4.44.2","webpack-dev-server":"^3.11.0"},"browser":{"./lib/adapters/http.js":"./lib/adapters/xhr.js"},"jsdelivr":"dist/axios.min.js","unpkg":"dist/axios.min.js","typings":"./index.d.ts","dependencies":{"follow-redirects":"^1.14.0"},"bundlesize":[{"path":"./dist/axios.min.js","threshold":"5kB"}]}');
 
 /***/ })
 
--- resources/js/main/Index.vue
+++ resources/js/main/Index.vue
@@ -1,12 +1,77 @@
 <template>
-    <div>
+    
+<div>
+    <nav class="navbbar navbbar-bg">
+        <router-link class="navbar-brand ml-3 nav-custom" :to="{name: 'home'}">Larpcons</router-link>
+        <button class="navbar-toggler" type="button">
+            <a id="menuburger" @click="MenuOpen" fill="none" stroke="currentColor">></a>    
+        </button>
+
+        <div class="hidden" id="menu">
+                <ul class="navbar-nav mr-auto">
+                
+                <li class="nav-item">
+                    <router-link class="nav-link nav-custom" :to="{name: 'calendar'}">Kalender</router-link>
+                </li>
+
+                
+               <li class="nav-item">
+                    <router-link class="nav-link nav-custom" :to="{name: 'user'}" v-if="user">Spieler-Bereich</router-link>
+                    <router-link class="nav-link disabled" :to="{name: 'user'}" v-else>Spieler-Bereich</router-link>
+                </li>
+    
+                <li class="nav-item">
+                    <router-link class="nav-link nav-custom" :to="{name: 'orga'}" v-if="orga_id">Orga-Bereich</router-link>
+                    <router-link class="nav-link disabled" :to="{name: 'orga'}" v-else>Orga-Bereich</router-link>
+                </li>
+
+                <li class="nav-item">
+                    <a 
+                        v-on:click = "logout"
+                        class="nav-link nav-custom" 
+                        :to="{name: 'home'}"
+                        v-if="user"
+                        >
+                        Logout</a>
+                    <router-link class="nav-link nav-custom" :to="{name: 'login'}" v-else>Login</router-link>
+                </li>
+                </ul>
+            </div> 
+
+    </nav>
+    <div class="background">
+            <div class="col-md-10 p-2 content">
+                <router-view></router-view>
+            </div>
+
+        </div>
+    </div>
+    <!-- <div class="fixed top-0 left-0 right-0 bg-white g-opacity-30 z-20 p-2" id="navbar">
+        <div class="md:flex md:flex-row md:justify-between">
+            <span class="inline md:w-64 md:text-xl">Larpcons</span>
+            <svg class="w-6 h-6 md:hidden cursor-pointer inline mr-4" id="menuburger" @click="MenuOpen" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        </div>
+        <div class="bg-white md:bg-transparent w-full hidden md:block" id="menu">
+            <ul class="flex flex-col md:flex-row md:justify-end">
+                <li class="text-xl my-4" @click="LinkOpen">
+                    <router-link class="px-4 custom menu-active-link" :to="{name: 'calendar'}">Kalender</router-link>
+                </li>
+            </ul>
+        </div>
+    </div> -->
+
+
+
+
+
+    <!-- <div>
          <nav class="navbar navbar-expand-sm  nav-bg">
             <router-link class="navbar-brand ml-4 nav-custom" :to="{name: 'home'}">Larpcons</router-link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarsExample03">
+            <div class="" id="navbarsExample03">
                 <ul class="navbar-nav mr-auto">
                 
                 <li class="nav-item">
@@ -43,7 +108,7 @@
             </div>
 
         </div>
-    </div>
+    </div> -->
 </template>
 
 <script>
@@ -54,7 +119,6 @@ export default {
             orga_id: null
         }
     },
-
     methods: {
         logout() {
             axios.post('/logout').then(response =>{
@@ -65,7 +129,21 @@ export default {
         async redirect() {
              //await this.$router.push({name: 'home'});
              window.location.reload();
-         }
+        },
+        MenuOpen() {
+        const menuburger = document.querySelector('#menuburger');
+        const menu = document.querySelector('#menu');
+        if (menu.classList.contains('hidden')){
+            menu.classList.remove('hidden');
+        } else {
+            menu.classList.add('hidden');
+        }
+        open.value = !open.value;
+        },
+        LinkOpen() {
+            const menu = document.querySelector('#menu');
+            menu.classList.add('hidden');
+        }
     },
 
     created() {
@@ -119,5 +197,8 @@ export default {
         margin-left: auto;
         margin-right: auto;
         color: white;
+    }
+    .hidden {
+        display: none;
     }
 </style>
